8acf1c7766d6614248a4ca224ceedfcb0683461e
julienasp <julien.aspirot@usherbrooke.ca>
2016-07-06 13:11:24 -0400

    new fonction

--- StartPoint.java
+++ StartPoint.java
@@ -5,12 +5,14 @@ import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.function.BiFunction;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import utils.Log;
@@ -47,16 +49,20 @@ public class StartPoint {
         //Chrono end!
         long time = System.nanoTime() - start;
         
-        //Writing down the result
-        File file=new File( RESULTPATH + "out-sequential.txt");   
-        try {
-            FileWriter fw=new FileWriter(file.getAbsoluteFile());
-            BufferedWriter bw=new BufferedWriter(fw);
-            bw.write(result.toString());
-            bw.close();
-        } catch (IOException ex) {
-            Logger.getLogger(StartPoint.class.getName()).log(Level.SEVERE, null, ex);
-        } 
+        try{
+            //Writing down the result
+            File file=new File( RESULTPATH + "out-sequential.txt");
+            BufferedWriter bw = null;
+            try {
+                FileWriter fw=new FileWriter(file.getAbsoluteFile());
+                bw=new BufferedWriter(fw);
+                bw.write(result.toString());    
+            } finally{
+                if(bw != null) bw.close();            
+            }
+        }catch(IOException ex){
+            Logger.getLogger(StartPoint.class.getName()).log(Level.SEVERE, null, ex);            
+        }
                 
         //Printing statistics        
         System.out.printf(Thread.currentThread().getName() + ": Sequential: " + listOfFiles.length + " files, " + result.size() +" words matching the length criteria, tasks took %.3f ms%n", time/1e6);
@@ -99,33 +105,45 @@ public class StartPoint {
         long time = System.nanoTime() - start;
         System.out.printf(Thread.currentThread().getName() + ": Prime thread: Tasks took %.3f ms to run%n", time/1e6);*/
     }
+    public static HashMap<String, Integer> mergeResult(HashMap<String,Integer> hm1, HashMap<String,Integer> hm2){    
+        HashMap<String, Integer> mergeResult = new HashMap();
+              
+        for (Map.Entry<String, Integer> e : hm2.entrySet()){
+            mergeResult.merge(e.getKey(), e.getValue(),(x, y) -> {return x + y;});
+        }
+        return mergeResult;
+    }
     
     public static HashMap<String, Integer> sequential(File[] filesToProcess){
         HashMap<String, Integer> result = new HashMap();
-        
+        Scanner scan = null;
         
         //Iterate through all the files
         for (File file : filesToProcess) {
             if (file.isFile()) {
-                try {
-                    Scanner scan = new Scanner(file); 
-                    
-                    //Read everyword of a file
-                    while(scan.hasNext()){
-                        String currentWord = scan.next();
+                try {   
+                    try {
+                        scan = new Scanner(file);
                         
-                        //If the word is big enough we process it
-                        if(currentWord.length() == StartPoint.WORDLENGTHCRTERIA){
-                            
-                            //If the word is already in the hashtable we incremente it
-                            if(result.containsKey(currentWord)){
-                                result.replace(currentWord, result.get(currentWord) + 1);
-                            }
-                            //Otherwise we add it
-                            else{
-                                result.put(currentWord, 1);
+                        //Read everyword of a file
+                        while(scan.hasNext()){
+                            String currentWord = scan.next();
+
+                            //If the word is big enough we process it
+                            if(currentWord.length() == StartPoint.WORDLENGTHCRTERIA){
+
+                                //If the word is already in the hashtable we incremente it
+                                if(result.containsKey(currentWord)){
+                                    result.replace(currentWord, result.get(currentWord) + 1);
+                                }
+                                //Otherwise we add it
+                                else{
+                                    result.put(currentWord, 1);
+                                }
                             }
                         }
+                    } finally{
+                        if (scan != null) scan.close(); 
                     }
                 } catch (FileNotFoundException ex) {
                     Logger.getLogger(StartPoint.class.getName()).log(Level.SEVERE, null, ex);
