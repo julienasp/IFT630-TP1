28f434aa7c07868858bc79744a1fe737aeb94a9d
julienasp <julien.aspirot@usherbrooke.ca>
2016-07-06 15:03:48 -0400

    remove statics + new Object

--- StartPoint.java
+++ StartPoint.java
@@ -17,14 +17,14 @@ import java.util.function.BiFunction;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import utils.Log;
+import core.FilesHandling;
 
 public class StartPoint {
     /************************************/
     /****  PUBLIC STATIC ATTRIBUTES *****/
     /************************************/
     public static final String FILESPATH = "files/";
-    public static final String RESULTPATH = "results/";
-    public static final int WORDLENGTHCRTERIA = 7;
+    public static final String RESULTPATH = "results/";    
     public static final int POOLSIZE = Runtime.getRuntime().availableProcessors();
     public static ExecutorService executor = Executors.newCachedThreadPool();
     //public static ExecutorService executor = Executors.newSingleThreadExecutor();
@@ -49,8 +49,9 @@ public class StartPoint {
         //Chrono start
         long senquentialStart = System.nanoTime();
         
+        FilesHandling fh = new FilesHandling(executor);
         //Sequential process executed
-        result = StartPoint.sequential(listOfFiles,0,listOfFiles.length-1);
+        result = fh.sequential(listOfFiles,0,listOfFiles.length-1);
         
         //Chrono end!
         long senquentialTime = System.nanoTime() - senquentialStart;
@@ -83,7 +84,7 @@ public class StartPoint {
             for (int threshold = 256; threshold <= 4096; threshold *= 2)
             {               
                 long parallelStart = System.nanoTime();
-                parallelResult = StartPoint.parallel(listOfFiles, 0, listOfFiles.length-1, threshold);
+                parallelResult = fh.parallel(listOfFiles, 0, listOfFiles.length-1, threshold);
                 long parallelTime = System.nanoTime() - parallelStart;
                 
                 try{
@@ -125,85 +126,5 @@ public class StartPoint {
        long globalTime = System.nanoTime() - globalChronoStart;  
 
         System.out.printf(Thread.currentThread().getName() + ": Prime thread: Tasks took %.3f ms to run%n", globalTime/1e6);
-    }
-    
-    public static HashMap<String, Integer> mergeResult(HashMap<String,Integer> hm1, HashMap<String,Integer> hm2){
-        Log.log(Thread.currentThread().getName() + ": mergeResult(): is being executed...");
-        HashMap<String, Integer> mergeResult = new HashMap();
-              
-        for (Map.Entry<String, Integer> e : hm2.entrySet()){
-            mergeResult.merge(e.getKey(), e.getValue(),(x, y) -> {return x + y;});
-        }
-        Log.log(Thread.currentThread().getName() + ": mergeResult(): has been executed.");
-        return mergeResult;
-    }
-    
-    public static HashMap<String, Integer> parallel(File[] filesToProcess,Integer start,Integer end,Integer threshold) throws InterruptedException, ExecutionException{
-        Log.log(Thread.currentThread().getName() + ": parallel(): is being executed...");
-        Log.log(Thread.currentThread().getName() + ": parallel(): Threashold is : " + threshold);
-        Log.log(Thread.currentThread().getName() + ": parallel(): start index is: " + start);
-        Log.log(Thread.currentThread().getName() + ": parallel(): end index is: " + end);
-        HashMap<String, Integer> result = new HashMap();
-        Integer n = (end + 1) - start; // length
-        if (n <= threshold){
-            return sequential(filesToProcess, start, end);
-        }        
-        Future<HashMap<String, Integer>> f0 = executor.submit(
-          new Callable<HashMap<String, Integer>>() {
-              @Override
-              public HashMap<String, Integer> call() throws InterruptedException, ExecutionException {                  
-                  Log.log(Thread.currentThread().getName() + ": f0 call(): is being executed...");
-                  return StartPoint.parallel(filesToProcess,start,end/2,threshold);
-              }
-          });
-        Future<HashMap<String, Integer>> f1 = executor.submit(
-          new Callable<HashMap<String, Integer>>() {
-              @Override
-              public HashMap<String, Integer> call() throws InterruptedException, ExecutionException{
-                  Log.log(Thread.currentThread().getName() + ": f1 call(): is being executed...");
-                  return StartPoint.parallel(filesToProcess,((end/2)+1),end,threshold);
-              }
-          });
-        Log.log(Thread.currentThread().getName() + ": parallel(): has been executed.");        
-        return mergeResult(f0.get(),f1.get());
-    }
-    
-    public static HashMap<String, Integer> sequential(File[] filesToProcess, Integer start,Integer end){
-        HashMap<String, Integer> result = new HashMap();
-        Scanner scan = null;
-        
-        //Iterate through all the files        
-        for (Integer i = start; i <= end; i++){
-            if (filesToProcess[i].isFile()) {
-                try {   
-                    try {
-                        scan = new Scanner(filesToProcess[i]);
-                        
-                        //Read everyword of a file
-                        while(scan.hasNext()){
-                            String currentWord = scan.next();
-
-                            //If the word is big enough we process it
-                            if(currentWord.length() == StartPoint.WORDLENGTHCRTERIA){
-
-                                //If the word is already in the hashtable we incremente it
-                                if(result.containsKey(currentWord)){
-                                    result.replace(currentWord, result.get(currentWord) + 1);
-                                }
-                                //Otherwise we add it
-                                else{
-                                    result.put(currentWord, 1);
-                                }
-                            }
-                        }
-                    } finally{
-                        if (scan != null) scan.close(); 
-                    }
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(StartPoint.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        }      
-        return result;
     }
 }
--- core/FilesHandling.java
+++ core/FilesHandling.java
@@ -4,15 +4,113 @@
  * and open the template in the editor.
  */
 package core;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utils.Log;
+
 
 /**
  *
  * @author JUASP-G73-Android
  */
-public class FilesHandling extends Thread {
-
-    @Override
-    public void run() {
-        super.run(); //To change body of generated methods, choose Tools | Templates.
+public class FilesHandling  {
+    public static final int WORDLENGTHCRTERIA = 7;
+    private ExecutorService executor;
+    
+    public FilesHandling(ExecutorService ex) {
+        executor = ex;
     }
+    
+    public HashMap<String, Integer> mergeResult(HashMap<String,Integer> hm1, HashMap<String,Integer> hm2){
+         Log.log(Thread.currentThread().getName() + ": mergeResult(): is being executed...");
+         HashMap<String, Integer> mergeResult = new HashMap();
+
+         for (Map.Entry<String, Integer> e : hm2.entrySet()){
+             mergeResult.merge(e.getKey(), e.getValue(),(x, y) -> {return x + y;});
+         }
+         Log.log(Thread.currentThread().getName() + ": mergeResult(): has been executed.");
+         return mergeResult;
+     }
+
+     public HashMap<String, Integer> parallel(File[] filesToProcess,Integer start,Integer end,Integer threshold) throws InterruptedException, ExecutionException{
+         Log.log(Thread.currentThread().getName() + ": parallel(): is being executed...");
+         Log.log(Thread.currentThread().getName() + ": parallel(): Threashold is : " + threshold);
+         Log.log(Thread.currentThread().getName() + ": parallel(): start index is: " + start);
+         Log.log(Thread.currentThread().getName() + ": parallel(): end index is: " + end);
+         HashMap<String, Integer> result = new HashMap();
+         Integer n = (end + 1) - start; // length
+         if (n <= threshold){
+             return sequential(filesToProcess, start, end);
+         }        
+         Future<HashMap<String, Integer>> f0 = executor.submit(
+           new Callable<HashMap<String, Integer>>() {
+               @Override
+               public HashMap<String, Integer> call() throws InterruptedException, ExecutionException {                  
+                   Log.log(Thread.currentThread().getName() + ": f0 call(): is being executed...");
+                   Log.log(Thread.currentThread().getName() + ": f0 call(): start index is: " + start);
+                   Log.log(Thread.currentThread().getName() + ": f0 call(): end index is: " + end);
+                   return parallel(filesToProcess,start,end/2,threshold);
+               }
+           });
+         Future<HashMap<String, Integer>> f1 = executor.submit(
+           new Callable<HashMap<String, Integer>>() {
+               @Override
+               public HashMap<String, Integer> call() throws InterruptedException, ExecutionException{
+                   Log.log(Thread.currentThread().getName() + ": f1 call(): is being executed...");
+                   Log.log(Thread.currentThread().getName() + ": f1 call(): start index is: " + start);
+                   Log.log(Thread.currentThread().getName() + ": f1 call(): end index is: " + end);
+                   return parallel(filesToProcess,((end/2)+1),end,threshold);
+               }
+           });
+         Log.log(Thread.currentThread().getName() + ": parallel(): has been executed.");        
+         return mergeResult(f0.get(),f1.get());
+     }
+
+     public HashMap<String, Integer> sequential(File[] filesToProcess, Integer start,Integer end){
+         HashMap<String, Integer> result = new HashMap();
+         Scanner scan = null;
+
+         //Iterate through all the files        
+         for (Integer i = start; i <= end; i++){
+             if (filesToProcess[i].isFile()) {
+                 try {   
+                     try {
+                         scan = new Scanner(filesToProcess[i]);
+
+                         //Read everyword of a file
+                         while(scan.hasNext()){
+                             String currentWord = scan.next();
+
+                             //If the word is big enough we process it
+                             if(currentWord.length() == WORDLENGTHCRTERIA){
+
+                                 //If the word is already in the hashtable we incremente it
+                                 if(result.containsKey(currentWord)){
+                                     result.replace(currentWord, result.get(currentWord) + 1);
+                                 }
+                                 //Otherwise we add it
+                                 else{
+                                     result.put(currentWord, 1);
+                                 }
+                             }
+                         }
+                     } finally{
+                         if (scan != null) scan.close(); 
+                     }
+                 } catch (FileNotFoundException ex) {
+                     
+                 }
+             }
+         }      
+         return result;
+     }
 }
